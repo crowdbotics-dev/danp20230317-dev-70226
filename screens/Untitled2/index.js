43c847bd39b3834af797f1efab1a57907a37606b
daniel palacios <daniel.palacios@crowdbotics.com>
2023-03-17 11:14:25 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,7 +1,16 @@
+import { universitiesapi_get_search_list } from "./../../store/universitiesAPI/universitiesapi_response_get_Searches.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import React from "react";
 import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
 
 const Untitled2 = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(universitiesapi_get_search_list({
+      param1: "value1"
+    }));
+  }, []);
   return <SafeAreaView style={styles.safeArea}>
         <ScrollView contentContainerStyle={{
       backgroundColor: '#f0f0f1',
